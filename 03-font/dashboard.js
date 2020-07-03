eaa114ca103de9ad523095f2879df7ab16fca6b8
Elijah Manor <elijah.manor@gmail.com>
2020-07-02 19:05:42 -0500

    Widen formatting

--- 03-font/dashboard.js
+++ 03-font/dashboard.js
@@ -1,44 +1,29 @@
-import React, {
-  useState,
-  useEffect,
-  useRef
-} from 'react'
+import React, { useState, useEffect } from 'react'
 import blessed from 'blessed'
 import { render } from 'react-blessed'
 
 const App = () => {
-  const [
-    count,
-    setCount
-  ] = useState(0)
-  const timer = useRef(null)
+  const [count, setCount] = useState(0)
   useEffect(() => {
-    timer.current = setTimeout(
-      () => setCount(count + 1),
-      1000
-    )
-    return () =>
-      clearTimeout(timer.current)
+    const timer = setTimeout(() => setCount(count + 1), 1000)
+    return () => clearTimeout(timer)
   }, [count])
 
-  let dateTime = new Date().toLocaleString(
-    'en-US',
-    {
-      month: 'long',
-      day: 'numeric',
-      year: 'numeric',
-      hour: '2-digit',
-      minute: '2-digit',
-      hour12: true
-    }
-  )
+  let dateTime = new Date().toLocaleString('en-US', {
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: true
+  })
 
   return (
     <box
       top="center"
       left="center"
-      width="50%"
-      height="50%"
+      width="65%"
+      height="65%"
       border={{ type: 'line' }}
       style={{
         border: { fg: 'blue' }
@@ -57,9 +42,6 @@ const screen = blessed.screen({
   title: 'Developer Dashboard'
 })
 
-screen.key(
-  ['escape', 'q', 'C-c'],
-  () => process.exit(0)
-)
+screen.key(['escape', 'q', 'C-c'], () => process.exit(0))
 
 render(<App />, screen)
