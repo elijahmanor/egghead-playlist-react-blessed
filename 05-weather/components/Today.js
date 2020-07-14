d7016ef0aba2084c8358dd0af51fbc11cee9adf5
Elijah Manor <elijah.manor@gmail.com>
2020-07-14 18:30:32 -0500

    End of 05-weather

--- 05-weather/components/Today.js
+++ 05-weather/components/Today.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import figlet from 'figlet'
 import useInterval from '@use-it/interval'
+import weather from 'weather-js'
+import util from 'util'
+
+const findWeather = util.promisify(weather.find)
 
 const FONTS = [
   'Straight',
@@ -17,13 +21,53 @@ const FONTS = [
   'Small Shadow'
 ]
 
-export default function Today({ updateInterval = 1000 }) {
+const formatWeather = ([results]) => {
+  const { location, current, forecast } = results
+  const degreeType = location.degreetype
+  const temperature = `${current.temperature}°${degreeType}`
+  const conditions = current.skytext
+  const low = `${forecast[1].low}°${degreeType}`
+  const high = `${forecast[1].high}°${degreeType}`
+
+  return `${temperature} and ${conditions} (${low} → ${high})`
+}
+
+export default function Today({
+  updateInterval = 900000,
+  search = 'Nashville, TN',
+  degreeType = 'F'
+}) {
   const [fontIndex, setFontIndex] = React.useState(0)
+  const [now, setNow] = React.useState(new Date())
+  const [weather, setWeather] = React.useState({
+    status: 'loading',
+    error: null,
+    data: null
+  })
+
+  const fetchWeather = React.useCallback(async () => {
+    setWeather({ status: 'loading', error: null, data: null })
+    let data
+    try {
+      data = await findWeather({ search, degreeType })
+      setWeather({ status: 'complete', error: null, data })
+    } catch (error) {
+      setWeather({ status: 'error', error, data: null })
+    }
+  }, [search, degreeType])
+
+  React.useEffect(() => {
+    fetchWeather()
+  }, [fetchWeather])
+
   useInterval(() => {
-    setFontIndex(fontIndex + 1)
+    fetchWeather()
   }, updateInterval)
 
-  const now = new Date()
+  useInterval(() => {
+    setNow(new Date())
+  }, 60000) // 1 minute
+
   const date = now.toLocaleString('en-US', {
     month: 'long',
     day: 'numeric',
@@ -53,7 +97,15 @@ export default function Today({ updateInterval = 1000 }) {
     >
       {`${date}
 
-${time}`}
+${time}
+
+${
+  weather.status === 'loading'
+    ? 'Loading...'
+    : weather.error
+    ? `Error: ${weather.error}`
+    : formatWeather(weather.data)
+}`}
     </box>
   )
 }
--- 05-weather/index.js
+++ 05-weather/index.js
@@ -1,7 +1,5 @@
 require('@babel/register')({
-  presets: [
-    ['@babel/preset-env'],
-    ['@babel/preset-react']
-  ]
+  presets: [['@babel/preset-env'], ['@babel/preset-react']],
+  plugins: ['@babel/plugin-transform-runtime']
 })
 require('./dashboard')
