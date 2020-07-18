511f1fc396f2a76309031837049294b1a9a7107e
Elijah Manor <elijah.manor@gmail.com>
2020-07-18 13:02:31 -0500

    Start of 08-position

---
+++ 08-position/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    node: true,
+    es2020: true
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 11,
+    sourceType: 'module'
+  },
+  plugins: ['react', 'react-hooks'],
+  rules: {
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/prop-types': 'off'
+  }
+}
