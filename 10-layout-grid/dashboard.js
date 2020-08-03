3232d42f614f5aee06ff57a8136b95b9f058c54c
Elijah Manor <elijah.manor@gmail.com>
2020-08-03 07:00:24 -0500

    Use Box at top level for grid

--- 10-layout-grid/dashboard.js
+++ 10-layout-grid/dashboard.js
@@ -2,6 +2,7 @@ import React from 'react'
 import blessed from 'blessed'
 import { render } from 'react-blessed'
 import Today from './components/Today'
+import Box from './components/Box'
 import TimeLog from './components/TimeLog'
 import Pomodoro from './components/Pomodoro'
 import RecentCommits from './components/RecentCommits'
@@ -12,16 +13,10 @@ const App = () => {
   return (
     <Grid rows={12} cols={12}>
       <Today updateInterval={900000} row={0} col={0} rowSpan={4} colSpan={6} />
-      <TimeLog row={4} col={0} rowSpan={8} colSpan={3} />
-      <Pomodoro row={4} col={3} rowSpan={8} colSpan={3} />
-      <RecentCommits
-        updateInterval={900000}
-        row={0}
-        col={6}
-        rowSpan={6}
-        colSpan={6}
-      />
-      <GitHub updateInterval={900000} row={6} col={6} rowSpan={6} colSpan={6} />
+      <Box label="Time Log" row={4} col={0} rowSpan={8} colSpan={3} />
+      <Box label="Pomodoro" row={4} col={3} rowSpan={8} colSpan={3} />
+      <Box label="Recent Commits" row={0} col={6} rowSpan={6} colSpan={6} />
+      <Box label="GitHub" row={6} col={6} rowSpan={6} colSpan={6} />
     </Grid>
   )
 }
