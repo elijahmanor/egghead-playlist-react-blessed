0c1146978972e3006cb8c39aa78ee3318863f3fc
Elijah Manor <elijah.manor@gmail.com>
2020-08-15 11:57:41 -0500

    Rearrange components to better fit timeline of lessons

--- 11-time-log/dashboard.js
+++ 11-time-log/dashboard.js
@@ -9,9 +9,9 @@ const App = () => {
   return (
     <Grid rows={12} cols={12}>
       <Today row={0} col={0} rowSpan={4} colSpan={6} updateInterval={900000} />
-      <Box label="Recent Commits" row={0} col={6} rowSpan={6} colSpan={6}></Box>
       <Box label="Time Log" row={4} col={0} rowSpan={8} colSpan={3}></Box>
       <Box label="Pomodoro" row={4} col={3} rowSpan={8} colSpan={3}></Box>
+      <Box label="Recent Commits" row={0} col={6} rowSpan={6} colSpan={6}></Box>
       <Box label="GitHub" row={6} col={6} rowSpan={6} colSpan={6}></Box>
     </Grid>
   )
